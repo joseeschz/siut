03213dccbcb9c0658ad9b99af519a6c4793a98cf
Carlos <karlos.antoni-1994@hotmail.com>
2016-01-09 23:14:58 -0600

    Se cambio el nombre de usuario en la conexión a la base de datos
    Se cambio el nombre de usuario root

--- src/java/control/conectionControl.java
+++ src/java/control/conectionControl.java
@@ -19,7 +19,7 @@ public class conectionControl {
     public Connection getConexion(){
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            return java.sql.DriverManager.getConnection("jdbc:mysql://10.10.40.5:3306/db_siut_dev", "root", "utsem_systems");
+            return java.sql.DriverManager.getConnection("jdbc:mysql://10.10.40.5:3306/db_siut_dev", "siut_dev", "utsem_systems");
         } catch (SQLException | ClassNotFoundException ex) {
             Logger.getLogger(conectionControl.class.getName()).log(Level.SEVERE, null, ex);
         }
